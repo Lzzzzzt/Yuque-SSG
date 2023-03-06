c471750b5a2f04c2fea4acf403531acc75b7d6c0
lzzzt <liuzitao0123@gmail.com>
2023-03-06 19:24:38 +0800

    ✨ feat(generator): Markdown格式化
    在写Markdown前，可以进行一些转换以及格式化

--- src/generator.rs
+++ src/generator.rs
@@ -1,6 +1,7 @@
-use std::{borrow::Cow, iter::zip, ops::Not, path::PathBuf};
+use std::{borrow::Cow, iter::zip, ops::Not, path::PathBuf, io::Write};
 
-use log::{debug, info};
+use comrak::{nodes::{AstNode, NodeValue, NodeHtmlBlock}, parse_document, Arena, ComrakOptions, format_commonmark};
+use log::{debug, info, warn};
 use tokio::{
     fs::{self, File},
     io::AsyncWriteExt,
@@ -139,13 +140,15 @@ impl<'n> Generator<'n> {
                     path.file_name().unwrap().to_string_lossy()
                 );
 
-                let mut async_file = File::from_std(file);
+                // let mut async_file = File::from_std(file);
 
-                async_file
+                file
                     .write_all(format!("# {}\n", doc.title).as_bytes())
-                    .await?;
+                    ?;
 
-                async_file.write_all(doc.body.as_bytes()).await?;
+                Formatter::new().parse(&doc.body).format(convert_codepen_link_to_iframe).write_to(file);
+
+                // async_file.write_all(doc.body.as_bytes()).await?;
 
                 debug!("Write File to: {}", path.display());
             }
@@ -179,4 +182,94 @@ impl<'a> From<GeneratorConfig<'a>> for Generator<'a> {
     fn from(value: GeneratorConfig<'a>) -> Self {
         Self::from_config(value)
     }
+}
+
+pub type FormatFunction<'a> = fn(&'a AstNode<'a>);
+
+pub struct Formatter<'a> {
+    root: Option<&'a AstNode<'a>>,
+    arena: Arena<AstNode<'a>>,
+    options: ComrakOptions,
+}
+
+impl<'a> Default for Formatter<'a> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<'a> Formatter<'a> {
+    pub fn new() -> Self {
+        let arena = Arena::new();
+        let mut options = ComrakOptions::default();
+
+        options.parse.default_info_string = Some("text".into());
+
+        Self { root: None, arena, options }
+    }
+
+    pub fn parse(&'a mut self, markdown: &str) -> &Self {
+        let root = parse_document(&self.arena, markdown, &self.options);
+        self.root = Some(root);
+        self
+    }
+
+    pub fn format(&self, func: FormatFunction<'a>) -> &Self {
+        if let Some(root) = self.root  {
+            Self::iter_nodes(root, func);
+            return self;
+        } 
+        warn!("Can not format before parse.");
+        self
+    }
+
+    fn iter_nodes<'n>(node: &'n AstNode<'n>, f: FormatFunction<'n>) {
+        f(node);
+        for c in node.children() {
+            Self::iter_nodes(c, f);
+        }
+    }
+
+    pub fn write_to(&self, file: std::fs::File) {
+        let mut file = file;
+
+        if let Some(root) = self.root {
+            format_commonmark(root, &self.options, &mut file).ok();
+            return;
+        }
+
+        warn!("Can not format before parse.");
+    }
+}
+
+fn convert_codepen_link_to_iframe<'a>(node: &'a AstNode<'a>) {
+    let mut url = String::new();
+        let mut content = String::new();
+
+        if let &NodeValue::Link(ref link) = &node.data.borrow().value {
+            url = String::from_utf8_lossy(&link.url).to_string();
+            if let &NodeValue::Text(ref text) = &node.first_child().unwrap().data.borrow().value {
+                content = String::from_utf8_lossy(text).to_string();
+            }
+        }
+
+        if url.is_empty().not() {
+            url::Url::parse(&url).ok().into_iter().for_each(|origin_url| {
+                        if let Some(domain) = origin_url.domain() {
+                            if domain.contains("codepen") {
+                                let literal = format!(r#"
+<iframe height="300" style="width: 100%;" scrolling="no" title="Untitled" src="{}" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">{}</iframe>
+                                "#, url, content).as_bytes().to_vec();
+
+                                node.children().for_each(|node| node.detach());
+
+                                node.data.borrow_mut().value =
+                                    NodeValue::HtmlBlock(NodeHtmlBlock {
+                                        block_type: 6,
+                                        literal,
+                                    })  
+                            }
+                        }
+                    })
+        }
 }
